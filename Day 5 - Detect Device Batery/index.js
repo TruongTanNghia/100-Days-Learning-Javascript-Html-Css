70fa01b6244426447b6e270af45bc076cabd3ed7
Vinh030579 <vinhphan10a1@gmail.com>
2023-11-06 23:06:57 +0700

    #28 Day 25 - JS Charts

--- Day 5 - Detect Device Batery/index.js
+++ Day 5 - Detect Device Batery/index.js
@@ -13,7 +13,7 @@ function initBattery() {
                 batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green-color"></i>`;
                 batteryLiquid.style.height = "103%";
             } else if (level <= 20 & !batt.charging) {
-                batteryStatus.innerHTML = `Low Charge <i class="ri-plug-line animated-red animated-red"></i>`;
+                batteryStatus.innerHTML = `  Low Charge <i class="ri-plug-line animated-red animated-red"></i>`;
             } else if (batt.charging) {
                 batteryStatus.innerHTML = `Charging ... <i class="ri-flashlight-line animated-green"></i>`;
             } else {
